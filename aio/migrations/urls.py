7380e4d5074bc3d31eaa23fbaacaa24757d34739
Suvarat Trivedi <suvTrivedi219@users.noreply.github.com>
2019-04-04 11:38:32 +0530

    more changes

--- aio/migrations/urls.py
+++ aio/migrations/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import *
+#from .views import *
 from . import views
+
+urlpatterns = [
+	path('', views.display_home, name='home'),
+]
--- aio/views.py
+++ aio/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from django.utils import *
+from django.utils.timezone import utc
+from django.contrib import messages
+
+def display_home(request):
+    return render(request, 'aio/home.html')
+
+#def add_animal(request):
+
+
 
 # Create your views here.
