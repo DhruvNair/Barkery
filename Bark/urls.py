0066b385f35068e100c3833e37771379b37615db
Dhruv Nair <nair.dhruv@gmail.com>
2019-04-04 11:53:28 +0530

    Models Debug

--- Bark/urls.py
+++ Bark/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+import aio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('aio/', include('aio.urls')),
-    
 ]
--- aio/models.py
+++ aio/models.py
@@ -9,15 +9,22 @@ class breed(models.Model):
     lifespan=models.IntegerField(default=0)
     height=models.FloatField(default=0.0)
     weight=models.FloatField(default=0.0)
+
 class animal(models.Model):
     breedname=models.ForeignKey(breed, on_delete=models.CASCADE)
     typename=models.CharField(max_length=100)
 
-
-    
 class color(models.Model):
     breedname=models.ManyToManyField(breed)
     color=models.CharField(max_length=0)
+
+class location(models.Model):
+    housenumber = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    pincode = models.IntegerField(default=0)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
 
 class temperament(models.Model):
     breedname=models.ManyToManyField(breed)
@@ -29,39 +36,21 @@ class pet(models.Model):
     gender = models.BooleanField(default=False)
     remarks = models.CharField(max_length=100)
     daysonbarkery = models.IntegerField(default=0)
-    location = models.ForeignKey(petlocation, on_delete=models.CASCADE)
-
-class petlocation(models.Model):
-    housenumber = models.CharField(max_length=100)
-    street = models.charField(max_length=100)
-    pincode = models.IntegerField(default=0)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
+    location = models.ForeignKey(location, on_delete=models.CASCADE)
 
 class disease(models.Model):
     pet=models.ForeignKey(pet, on_delete=models.CASCADE)
     disease=models.CharField(max_length=100)
 
-
-
-#### SHop tables ####
-class brand:
+#### Shop tables ####
+class brand(models.Model):
     brandname=models.CharField(max_length=100)
     brandrating=models.FloatField(default=0.0)
     email=models.CharField(max_length=100)
-    contact=models.CHarField(max_length=10)
-    
-class brandlocation(models.Model):
-    brand=models.ForeignKey(brand, on_delete=models.CASCADE)
-    housenumber = models.CharField(max_length=100)
-    street = models.charField(max_length=100)
-    pincode = models.IntegerField(default=0)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
+    contact=models.CharField(max_length=10)
+    location = models.ForeignKey(location, on_delete=models.CASCADE)
 
-class item:
+class item(models.Model):
     itemname = models.CharField(max_length=100)
     animal = models.ForeignKey(animal, on_delete=models.CASCADE)
     itemtype = models.CharField(max_length=100)
