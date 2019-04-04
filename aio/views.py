0558645a4c1ec004508ec21e19ec10488ecd93c9
Suvarat Trivedi <suvTrivedi219@users.noreply.github.com>
2019-04-04 18:33:32 +0530

    views 2

--- aio/views.py
+++ aio/views.py
@@ -45,7 +45,7 @@ def add_animal(request):
                 cursor = connection.cursor()
                 cursor.execute('INSERT INTO `aio_animal` (`animal_type`,`animal_breed`,`avglife`,`height`,`weight`,`color`,`temperament`) VALUES ("{}","{}",{},{},{},"{}","{}");'.format(animal_type,animal_breed,avglife,height,weight,color,temperament))
                 connection.commit()
-                print ("Record inserted successfully into python_users table")
+                print ("Record inserted successfully into aio_animals table")
                 messages.success(request, f'Stock successfully added.')
                 return redirect('home')
             except mysql.connector.Error as error :
@@ -56,13 +56,13 @@ def add_animal(request):
 	 }
     return render(request, "aio/add_animal.html", context)
 
-'''def add_item(request):
+def add_item(request):
     form = AddItem()
     if request.method == 'POST':
         form = AddItem(request.POST)
         if form.is_valid():
-            name=str(form.cleaned_data['name'])
-            type=str(form.cleaned_data['type'])
+            item_name=str(form.cleaned_data['name'])
+            item_type=str(form.cleaned_data['type'])
             description=int(form.cleaned_data['description'])
             cost=float(form.cleaned_data['cost'])
             rating=float(form.cleaned_data['rating'])
@@ -73,17 +73,19 @@ def add_animal(request):
                 cursor = connection.cursor()
                 cursor.execute('SELECT * FROM aio_animal WHERE animal_type = %s AND animal_breed = %s', [animal_type,animal_breed])
                 result = dictfetchall(cursor)
-                cursor.execute('SELECT * FROM aio_sbrand WHERE animal_type = %s AND animal_breed = %s', [animal_type,animal_breed])
-                result = dictfetchall(cursor)
-                cursor.execute('INSERT INTO `aio_item` (`name`,`type`,`description`,`cost`,`rating`,`animal`,`brand`) VALUES ("{}","{}",{},{},{},"{}","{}");'.format(animal_type,animal_breed,avglife,height,weight,color,temperament))
+                animalid=result[0].id
+                cursor.execute('SELECT * FROM aio_brand WHERE brand_name = %s', [brand])
+                result1 = dictfetchall(cursor)
+                brandid=result[0].id
+                cursor.execute('INSERT INTO `aio_item` (`item_name`,`item_type`,`description`,`cost`,`rating`,`animal`,`brand`) VALUES ("{}","{}","{}",{},{},{},{});'.format(item_name,item_type,description,cost,rating))
                 connection.commit()
-                print ("Record inserted successfully into python_users table")
+                print ("Record inserted successfully into aio_items table")
                 messages.success(request, f'Stock successfully added.')
                 return redirect('home')
             except mysql.connector.Error as error :
                 connection.rollback() #rollback if any exception occured
-                print("Failed inserting record into aio_animals table {}".format(error))
-'''
+                print("Failed inserting record into aio_items table {}".format(error))
+
 
 
 # Create your views here.
